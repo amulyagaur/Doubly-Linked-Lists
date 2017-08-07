31dee5c438a51521108d36945d09c78a19bd9a97
Amulya Gaur <amulyagaur111@gmail.com>
2017-08-07 18:29:30 +0530

    Update dll-core.cpp

--- dll-core.cpp
+++ dll-core.cpp
@@ -7,65 +7,117 @@ struct node
 	node* prev;
 	node* next;
 };
-struct node* head;
 
-//function to dynamically allocate a new node
-node* new_node(int x)
+node* insert_head(node* head,int x)
 {
 	node* temp = new node();
-	temp->data = x;
-	temp->prev = NULL;
-	temp->next = NULL;
-	return temp;
-}
-
-//function to insert at the head of the doubly-linkedlist
-void insert_at_head(int x)
-{
-	node* temp = new_node(x);
+	temp->data =x;
 	if(head==NULL)
 	{
 		head = temp;
-		return;
+		temp->prev = NULL;
+		temp->next = NULL;
+		return head;
 	}
+	temp->prev = NULL;
 	head->prev = temp;
 	temp->next=head;
 	head = temp;
+	return head;
 }
 
-//function to print the doubly linked list
-
-void print_f()
+void print_rec(node* head)
 {
-	node* temp = head;
-	while(temp!=NULL)
+	while(head !=NULL)
 	{
-		cout<<temp->data<<" ";
-		temp = temp->next;
+		cout<<head->data<<" ";
+		head = head->next;
 	}
 	cout<<endl;
+	return;
 }
 
-//function to print the reverse of doubly-linkedlist 
+node* insert_end(node* head,int x)
+{
+	node* temp = new node();
+	temp->data =x;
+	if(head==NULL)
+	{
+		head = temp;
+		temp->prev = NULL;
+		temp->next = NULL;
+		return head;
+	}
+	node* ptr = head;
+	while(ptr->next !=NULL)
+		ptr = ptr->next;
+	ptr->next = temp;
+	temp->prev = ptr;
+	temp->next = NULL;
+	return head;
+}
 
-void print_r()
+node* insert_at_pos(node* head,int x,int pos)
+{
+	node* temp = new node();
+	temp->data =x;
+	if(pos==1)
+	{
+		
+		temp->prev = NULL;
+		temp->next = head;
+		head = temp;
+		return head;
+	}
+	node* ptr = head;
+	for(int i=1;i<(pos-1);i++)				//iterating to previous position
+		ptr = ptr->next;
+	temp->prev = ptr;
+	temp->next = ptr->next;
+	ptr->next->prev = temp;
+	ptr->next = temp;
+	return head;
+}
+
+node* delete_front(node* head)
 {
 	node* temp = head;
-	while(temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	cout<<"reverse is"<<endl;
-	while(temp!=NULL)
-	{
-		cout<<temp->data<<" ";
-		temp = temp->prev;
-	}
-	cout<<endl;
+	head = head->next;
+	head->prev =  NULL;
+	delete temp;
+	return head;
 }
 
-//function to reverse a doubly linked list
-void reverse()
+node* delete_end(node* head)
+{
+	node* ptr=head;
+	while(ptr->next !=NULL)
+		ptr=ptr->next;
+	ptr->prev->next=NULL;
+	delete ptr;
+	return head;
+}
+
+node* delete_at_pos(node* head,int pos)
+{
+	if(pos==1)
+		delete_front(head);
+	else
+	{
+		node* ptr=head;
+		for(int i=1;i<(pos);i++)
+			ptr=ptr->next;                              //iterating to that position
+		node* p=ptr->prev;
+		node* q = ptr->next;
+		p->next=q;
+		q->prev = p;
+		delete ptr;
+		return head;
+	}
+	
+}
+
+node* reverse(node* head)
 {
 	node* temp = head;
 	while(temp!=NULL)
@@ -74,38 +126,36 @@ void reverse()
 		head = temp;
 		temp=temp->prev;
 	}
+	return head;
 }
-
-//function to insert at the tail of doubly linked list
-void insert_at_tail(int x)
-{
-	node* temp = new_node(x);
-	node* temp1 = head;
-	if(head==NULL)
-	{
-		head = temp;
-		return;
-	}
-	//else iterate to the last node 
-
-	while(temp1->next != NULL)
-		temp1=temp1->next;
-	temp1->next = temp;
-	temp->prev = temp1;
-}
-
-
 
 int main()
 {
-    head = NULL;
-    insert_at_head(2); print_f(); print_r();
-    insert_at_head(4); print_f(); print_r();	
-    insert_at_head(6); print_f(); print_r();
-    insert_at_head(8); print_f(); print_r();
-    reverse();
-    print_r();
-    insert_at_tail(1);
-    print_f();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    struct node* head = NULL;
+    head=insert_head(head,5);
+    head=insert_head(head,4);
+    head=insert_head(head,3);
+    head=insert_head(head,2);
+    head=insert_head(head,1);
+    print_rec(head);
+    head=insert_end(head,6);
+    head=insert_end(head,6);
+    head=insert_end(head,6);
+    head=insert_end(head,6);
+    print_rec(head);
+    head = insert_at_pos(head,9,2);
+    print_rec(head);
+    head = insert_at_pos(head,9,1);
+    print_rec(head);
+    head = delete_end(head);
+    print_rec(head);
+    head = delete_front(head);
+    print_rec(head);
+    head = delete_at_pos(head,1);
+    print_rec(head);
+    head = reverse(head);
+    print_rec(head);
     return 0;
 }
